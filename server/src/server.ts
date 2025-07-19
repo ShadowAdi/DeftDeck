bbe2b3f1b03decc8b22cf8072a27fbf0e049509a
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-19 11:37:23 +0530

    Added Health Router

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,13 @@ import "./config/DotEnvConfig.js";
 import express from "express";
 import { CorsConfig } from "./config/corsConfig.js";
 import { AppConnect } from "./config/appConfigRunner.js";
+import { healthRouter } from "./routes/healthRouter.js";
 
 const app = express();
 
 CorsConfig(app);
 app.use(express.json());
+
+app.use("/api/health", healthRouter);
 
 AppConnect(app);
