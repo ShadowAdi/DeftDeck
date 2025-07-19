c3722eb1b7eb3f350bf8ab4ab2d96072a015b90b
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-19 22:24:08 +0530

    added controller template

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,2 +1,8 @@
-import { Request } from "express";
+import { NextFunction, Request, Response } from "express";
+import { CustomTryCatch } from "../utils/CustomTryCatch.js";
 
+export const GetAllUsers = CustomTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
+    
+  }
+);
