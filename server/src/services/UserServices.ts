80f5e864fa7f4f221057348c82ceca278f10b325
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-20 12:06:56 +0530

    added Get User Service

--- server/src/services/UserServices.ts
+++ server/src/services/UserServices.ts
@@ -16,3 +16,14 @@ export const GetAllUsersService = async (
     throw new AppError(`Error Fetching Users From DB`, 500);
   }
 };
+
+export const GetUserService = async (userId: string) => {
+  try {
+    const user = await UserModel.findById(userId);
+    return user;
+  } catch (error) {
+    logger.error(`Failed to get user with this id: ${userId} ` + error);
+    console.error(`Failed to get user with this id: ${userId} `, error);
+    throw new AppError(`Failed to get user with this id: ${userId} `, 500);
+  }
+};
