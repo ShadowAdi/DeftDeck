0d9728e91b9c6f5cd47edc6d02321cf8bf89861c
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-30 19:33:28 +0530

    making cards more robust

--- server/src/schemas/CardSchemas.ts
+++ server/src/schemas/CardSchemas.ts
@@ -2,40 +2,80 @@ import mongoose, { Types } from "mongoose";
 
 export const CardSchema = new mongoose.Schema(
   {
-    cardUrl: {
+    cardType: {
       type: String,
+      enum: ["LINK", "INFO", "CHECKLIST", "SNIPPET", "FILE"],
       required: true,
+      default: "LINK",
     },
-    cardColor: {
-      type: String,
-      default: "",
-    },
-    cardIcon: {
-      type: String,
-    },
+
     cardTitle: {
       type: String,
       required: true,
     },
+
     cardDescription: {
       type: String,
     },
+
+    // For LINK
+    cardUrl: {
+      type: String,
+    },
+
+    // For INFO
+    cardText: {
+      type: String,
+    },
+
+    // For CHECKLIST
+    cardChecklist: [
+      {
+        task: { type: String },
+        done: { type: Boolean, default: false },
+      },
+    ],
+
+    // For SNIPPET
+    cardCode: {
+      language: { type: String },
+      content: { type: String },
+    },
+
+    // For FILE
+    cardFileUrl: {
+      type: String,
+    },
+
+    cardColor: {
+      type: String,
+      default: "",
+    },
+
+    cardIcon: {
+      type: String,
+    },
+
     cardImage: {
       type: String,
     },
+
     cardTags: {
       type: [String],
       default: [],
     },
+
     isEditable: {
       type: Boolean,
       default: false,
     },
+
     creator: {
       type: Types.ObjectId,
       ref: "User",
       required: true,
     },
+
     cardPanel: {
       type: Types.ObjectId,
       ref: "Panel",
