51bf9217734cdc0e350aabb88ed1ce822f13e122
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-01 08:54:38 +0530

    added login page

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/components/global/LoginForm";
+import React from "react";
+
+const Login = () => {
+  return (
+    <section className="flex w-full h-full flex-col space-y-5 max-w-lg items-start my-auto justify-center">
+      <h1 className="text-3xl text-black font-semibold ">Login User</h1>
+      <LoginForm />
+    </section>
+  );
+};
+
+export default Login;
